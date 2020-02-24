d9c7a813d4603a436e83b66e22acd410544dbfda
Nanshy <nanshy@yandex.ru>
2020-02-24 22:47:52 +0500

    Заполнение массива masB

--- Laba1.py
+++ Laba1.py
@@ -14,7 +14,15 @@ def GetDataTest():
 Функция, которая заполняет массив
 '''
 def ZapolnitMassiv(mas : np.ndarray):
-    pass
+    masB = np.zeros((5))
+    temp = np.zeros((5))
+    for i in range(mas.shape[0]):
+        temp[i] = sum(mas[i])
+        if (temp[i]%2==0):
+            masB[i] = 2
+        else:
+            masB[i] = 1
+    return(masB)
 
 
 '''
