da482a08e25188ee11456bf68409ad5b730af254
Asf-artur <asf-artur@yandex.ru>
2020-02-24 22:29:42 +0500

    две новые функции

--- Laba1.py
+++ Laba1.py
@@ -1,10 +1,26 @@
 import numpy as np
 import pandas as pd
+import math
+
+'''
+Функция, которая заполняет массив
+'''
+def ZapolnitMassiv(mas : np.ndarray):
+    pass
+
+
+'''
+Факториал
+'''
+def Factorial(mas : np.ndarray):
+    pass
 
 
 def main():
     masA = np.zeros((5, 5))
     print(masA)
+    masA = ZapolnitMassiv(masA)
+    masA = Factorial(masA)
 
 
 main()
